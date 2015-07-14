3d683fae8a4b43a6334c9d2423f312bcdbde2781
oliveagle <oliveagle@gmail.com>
2015-07-14 15:02:30 +0800

    ubuntu fix

--- daemon_linux.go
+++ daemon_linux.go
@@ -6,11 +6,20 @@
 package daemon
 
 import (
+	"github.com/oliveagle/goos"
 	"os"
 )
 
 // Get the daemon properly
 func newDaemon(name, description string) (Daemon, error) {
+
+	ID, _, _, _ := goos.GetOSVersion()
+
+	if ID == "ubuntu" {
+		// do ubuntu things
+		return &ubuntuSysVRecord{name, description}, nil
+	}
+
 	if _, err := os.Stat("/run/systemd/system"); err == nil {
 		return &systemDRecord{name, description}, nil
 	}
